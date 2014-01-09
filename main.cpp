484fccaecbd5314768b766b9af9091cd4da1ed8b
Dogukan Cagatay <dcagatay@gmail.com>
2014-01-09 15:05:44 +0200

    program argument part is removed

--- main.cpp
+++ main.cpp
@@ -1,41 +1,16 @@
 #include <iostream>
-#include <boost/program_options.hpp>
 #include "api/graph.hpp"
 
 typedef struct global_parameters {
     int nshards;
     int membudget;
 } params;
-namespace po = boost::program_options;
+
 using namespace std;
 
 int main(int argc, char** argv){
     /* keep global program parameters in par */
     params par;
-
-    /* --- getting arguments with boost program_options --- */
-    po::options_description desc("Allowed options");
-    desc.add_options()
-        ("help,h", "Produces help message")
-        ("nshards,s", po::value<int>(&(par.nshards))->default_value(-1),
-                  "Number of memory shards the graph is going to be partitioned.")
-        ("membudget,m", po::value<int>(&(par.membudget))->default_value(800),
-                  "Memory amount in MB that can be used by the program. Set to 70% of your total memory.")
-        ;
-
-    po::positional_options_description p;
-    p.add("help", -1);
-
-    po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
-    po::notify(vm);
-
-    if (vm.count("help")) {
-        std::cout << "Usage: options_description [options]" << std::endl;
-        std::cout << desc;
-        return 0;
-    }
-    /* -------------------------------------------------- */
 
     // Sample graph
     graph_t<int> g;
