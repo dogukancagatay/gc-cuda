5ac8fab2a04ffde79d9de3dfff3ed5bf6f06a854
Dogukan Cagatay <dcagatay@gmail.com>
2014-01-09 22:31:45 +0200

    number of shards are calculated and std::unordered_map is changed to boost for performance.

--- preprocessing/sharding.hpp
+++ preprocessing/sharding.hpp
@@ -5,18 +5,26 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <unordered_map>
+#include <boost/unordered_map.hpp>
+//#include <unordered_map>
 #include <utility>
+#include <math.h>
 
-//#include "api/graph.h"
+#define MB_TO_BYTES 1024*1024
 
-void foo(char* gfilename){
+#include "../api/utility.hpp"
+
+typedef boost::unordered_map<int, int> hash_t;
+
+void shard_graph(params* par, char* gfilename){
     /* Open the graph file */
     std::ifstream gfile(gfilename);
-    std::string eline;
-    std::unordered_map<int,int> in_edge_count; // node <-- number of in edges
+    hash_t in_edge_count; // node <-- number of in edges
+
+    int num_edges = 0;
 
     if(gfile.is_open()){
+        std::string eline;
         while(getline(gfile, eline)){
             /* get an edge from the file */
             std::istringstream buff(eline);
@@ -26,14 +34,13 @@ void foo(char* gfilename){
             buff >> to;
 
             if(in_edge_count.find(to) == in_edge_count.end()) { //if the node is encountered for the first time
-                in_edge_count.insert(std::make_pair(to,1));
+                in_edge_count.insert(hash_t::value_type(to,1));
             }
             else {
                 in_edge_count[to] += 1;
             }
         }
 
-        int num_edges = 0;
         for(auto it = in_edge_count.begin(); it != in_edge_count.end(); ++it){
             std::cout << it->first << " : " << it->second << std::endl;
             num_edges += it->second;
@@ -44,7 +51,24 @@ void foo(char* gfilename){
 
         gfile.close();
     }
-    else std::cout << "Unable to open file" << std::endl;
+    else {
+        std::cout << "Unable to open file" << std::endl;
+        exit(1); //exit with error
+    }
+
+    /* calculate number of shards needed */
+    int membudget_b = par->mem_budget * MB_TO_BYTES;
+    par->max_num_edges = (int)floor((double)membudget_b / (double)par->edge_mem_cost);
+    par->num_shards = (int)ceil((double)num_edges / (double)par->max_num_edges);
+
+
+    std::cout << "Max number of edges per shard = " << par->max_num_edges << std::endl;
+    std::cout << "Number of shards = " << par->num_shards << std::endl;
+
+    /* create files for shards */ 
+    /* assign vertices to shards */
+    /* scan the graph file and start writing to shards */
+
 }
 
 #endif
